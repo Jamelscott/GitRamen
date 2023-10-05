6e701af5966b56b7e5669c44ebbdbfc5d9490ead
Jamel-bbox <jamel@balloonbox.io>
2023-10-05 12:00:09 -0700

    troubleshooting

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,123 +1,131 @@
 // imports
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const db = require('../models')
-const bcrypt = require('bcrypt')
-const cryptojs = require('crypto-js');
-const { append, redirect } = require('express/lib/response');
-const axios = require('axios');
-const { sequelize } = require('../models');
-var methodOverride = require('method-override');
+const db = require("../models");
+const bcrypt = require("bcrypt");
+const cryptojs = require("crypto-js");
+const { append, redirect } = require("express/lib/response");
+const axios = require("axios");
+const { sequelize } = require("../models");
+var methodOverride = require("method-override");
 
-require('dotenv').config()
-
+require("dotenv").config();
 
 // Get to update page
-router.post('/update', (req, res)=>{
-
-    const reviewData = req.body
-    res.render('restaurants/update.ejs',{reviewData: reviewData} )
-})
+router.post("/update", (req, res) => {
+  const reviewData = req.body;
+  res.render("restaurants/update.ejs", { reviewData: reviewData });
+});
 //updating reviews
-router.put('/update', async (req, res)=>{
-    console.log(req.body)
-    const restId = req.body.restId
-    console.log(restId)
-    const newComment = req.body.newComment
-    const newRating = req.body.newRating
-    const commentId = req.body.commentId
+router.put("/update", async (req, res) => {
+  console.log(req.body);
+  const restId = req.body.restId;
+  console.log(restId);
+  const newComment = req.body.newComment;
+  const newRating = req.body.newRating;
+  const commentId = req.body.commentId;
 
-    const update = await db.review.update({
-        comment: newComment,
-        rating: newRating 
-    }, {
-        where: {id: commentId}
-    })
+  const update = await db.review.update(
+    {
+      comment: newComment,
+      rating: newRating,
+    },
+    {
+      where: { id: commentId },
+    }
+  );
 
-    res.redirect(`/restaurants/${restId}`)
-
-})
+  res.redirect(`/restaurants/${restId}`);
+});
 
 // Route to Restaurant Index page
-router.get('/', (req, res)=>{
-    const url = `https://api.yelp.com/v3/businesses/search?location=vancouver&categories=ramen`
-    const header = {headers: {Authorization: 'Bearer ' + process.env.YELP_ACCESS}}
-    axios.get(url, header)
-    .then(function (response) {
-        // handle success
-        let restaurants = response.data.businesses
+router.get("/", (req, res) => {
+  const url = `https://api.yelp.com/v3/businesses/search?location=vancouver&categories=ramen`;
+  const header = {
+    headers: { Authorization: "Bearer " + process.env.YELP_ACCESS },
+  };
+  console.log(typeof url, url);
+  axios.get(url, header).then(function (response) {
+    // handle success
+    let restaurants = response.data.businesses;
 
-        res.render('restaurants', {restaurant: restaurants})
-        // res.send(restaurants)
-    })
-})
+    res.render("restaurants", { restaurant: restaurants });
+    // res.send(restaurants)
+  });
+});
 
 // Route to Single Restaurant page
-router.get('/:id', (req, res)=>{
+router.get("/:id", (req, res) => {
+  const url = `https://api.yelp.com/v3/businesses/search?location=vancouver&categories=ramen`;
+  const header = {
+    headers: { Authorization: "Bearer " + process.env.YELP_ACCESS },
+  };
+  axios.get(url, header).then(async function (response) {
+    // handle success
+    // create variables
+    let currentRestaurant = response.data.businesses[req.params.id - 1];
+    // console.log(currentRestaurant)
+    //create object of all reviews for single restaurant
+    const restaurantAndReviews = await db.restaurant.findOne({
+      where: { yelpid: currentRestaurant.id },
+      include: {
+        model: db.review,
+        include: db.user,
+      },
+    });
+    const restaurantIdentifier = req.params.id;
+    console.log(restaurantIdentifier);
 
-    const url = `https://api.yelp.com/v3/businesses/search?location=vancouver&categories=ramen`
-    const header = {headers: {Authorization: 'Bearer ' + process.env.YELP_ACCESS}}
-    axios.get(url, header)
-    .then(async function (response) {
-        // handle success
-        // create variables
-        let currentRestaurant = response.data.businesses[req.params.id-1]
-        // console.log(currentRestaurant)
-        //create object of all reviews for single restaurant
-            const restaurantAndReviews = await db.restaurant.findOne({
-                where: {yelpid: currentRestaurant.id},
-                include: {
-                    model: db.review,
-                    include: db.user
-                }
-            })
-            const restaurantIdentifier = req.params.id
-            console.log(restaurantIdentifier)
-   
-
-            // console.log(findUser)
-            if(restaurantAndReviews === null){
-                res.render('restaurants/show.ejs', {restaurant:currentRestaurant, restId: restaurantIdentifier, reviews: null, userData: res.locals.user})
-            } else {
-                res.render('restaurants/show.ejs', {restaurant:currentRestaurant, restId: restaurantIdentifier, reviews: restaurantAndReviews.reviews, userData: res.locals.user})
-            }
-
-
-    })
-})
+    // console.log(findUser)
+    if (restaurantAndReviews === null) {
+      res.render("restaurants/show.ejs", {
+        restaurant: currentRestaurant,
+        restId: restaurantIdentifier,
+        reviews: null,
+        userData: res.locals.user,
+      });
+    } else {
+      res.render("restaurants/show.ejs", {
+        restaurant: currentRestaurant,
+        restId: restaurantIdentifier,
+        reviews: restaurantAndReviews.reviews,
+        userData: res.locals.user,
+      });
+    }
+  });
+});
 
 // route to post a review
 
-router.post('/review', async (req, res)=>{
-    // console.log(req.body.yelpIndex, req.body.restaurant)
-    // this is allowing me to see the cookies/User Id
-    const decryptedId = cryptojs.AES.decrypt(req.cookies.userId, process.env.SECRET)
-    const decryptedIdString = decryptedId.toString(cryptojs.enc.Utf8)
-    // Here I am about to make an entry into the resturant DB
-    const [restaurant, create] = await db.restaurant.findOrCreate({
-        where: {yelpid: req.body.yelpIndex, name: req.body.restaurant}
-    })
-    const newReview = await db.review.create({
-        comment: req.body.review,
-        rating: req.body.rating
-    })
-    
-    restaurant.addReview(newReview)
-    res.locals.user.addReview(newReview)
-    res.redirect('back')
-    // console.log(restaurant.reviews) // reviews for all of that restaurant
-    
+router.post("/review", async (req, res) => {
+  // console.log(req.body.yelpIndex, req.body.restaurant)
+  // this is allowing me to see the cookies/User Id
+  const decryptedId = cryptojs.AES.decrypt(
+    req.cookies.userId,
+    process.env.SECRET
+  );
+  const decryptedIdString = decryptedId.toString(cryptojs.enc.Utf8);
+  // Here I am about to make an entry into the resturant DB
+  const [restaurant, create] = await db.restaurant.findOrCreate({
+    where: { yelpid: req.body.yelpIndex, name: req.body.restaurant },
+  });
+  const newReview = await db.review.create({
+    comment: req.body.review,
+    rating: req.body.rating,
+  });
 
-})
+  restaurant.addReview(newReview);
+  res.locals.user.addReview(newReview);
+  res.redirect("back");
+  // console.log(restaurant.reviews) // reviews for all of that restaurant
+});
 // Deleting comments
-router.delete('/:id', (req, res)=>{
-    
-    db.review.destroy({
-        where: {id: req.body.id}
-    })
+router.delete("/:id", (req, res) => {
+  db.review.destroy({
+    where: { id: req.body.id },
+  });
 
-    res.redirect('back')
-
-})
+  res.redirect("back");
+});
 
 module.exports = router;

478b40c87b711cf83f8499809fb2dd7ae5f5e5d2
Jamel-bbox <jamel@balloonbox.io>
2023-10-26 00:33:08 -0700

    test

--- models/index.js
+++ models/index.js
@@ -11,8 +11,10 @@ const db = {};
 let sequelize;
 console.log(process.env[config.use_env_variable]);
 if (config.use_env_variable) {
+  console.log('hi')
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  console.log('hi')
   sequelize = new Sequelize(
     config.database,
     config.username,
